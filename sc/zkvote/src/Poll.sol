0dd2385ef1d52082e35c4a013ee56eec3e896fd2
zanpatryk <patryk.zan@wp.pl>
2025-07-05 04:23:15 +0200

    Poll setup update

--- sc/zkvote/src/Poll.sol
+++ sc/zkvote/src/Poll.sol
@@ -25,26 +25,29 @@ contract Poll {
     error Poll__NotAGovernor();
     error Poll__UserAlreadyVoted();
     error Poll__InvalidOption();
+    error Poll__UserNotOldEnough();
+    error Poll_UserWithInvalidNationality();
 
     // State Variables
-    address private immutable i_governor;
+    address private immutable GOVERNOR;
 
-    string private s_title;
-    State private s_state;
+    string private title;
+    State private state;
 
-    string[] private s_options;
-    mapping(uint256 option => uint256 voteCounter) public s_votes;
+    string[] private options;
+    mapping(uint256 option => uint256 voteCounter) public votes;
 
-    address[] private s_participantList;
-    mapping(address paticipant => bool isRegistered) public s_isRegistered;
-    mapping(address participant => bool isVerified) public s_isVerified;
-    mapping(address paticipant => bool hasVoted) public s_hasVoted;
+    address[] private participantList;
+    mapping(address paticipant => bool isRegistered) public isRegistered;
+    mapping(address paticipant => bool hasVoted) public hasVoted;
 
-    uint256 private s_startTime;
-    uint256 private s_endTime;
-    uint256 private s_duration;
+    uint256 private startTime;
+    uint256 private endTime;
+    uint256 private duration;
 
-    address private immutable i_owner;
+    mapping(string country => bool isAllowed) private countriesAllowed;
+
+    address private immutable OWNER;
 
     // Events
     event PollStarted(uint256 indexed startTime, uint256 indexed endTime);
@@ -55,21 +58,21 @@ contract Poll {
 
     // Modifiers
     modifier onlyOwner() {
-        if (msg.sender != i_owner) {
+        if (msg.sender != OWNER) {
             revert Poll__NotAnOwner();
         }
         _;
     }
 
     modifier onlyGovernor() {
-        if (msg.sender != i_governor) {
+        if (msg.sender != GOVERNOR) {
             revert Poll__NotAGovernor();
         }
         _;
     }
 
     modifier inState(State expected) {
-        if (s_state != expected) {
+        if (state != expected) {
             revert Poll__NotExpectedState();
         }
         _;
@@ -81,7 +84,13 @@ contract Poll {
      * @param _options  The array of option labels
      * @param _duration Duration in seconds for which the poll runs once started
      */
-    constructor(string memory _title, string[] memory _options, uint256 _duration, address _owner) {
+    constructor(
+        string memory _title,
+        string[] memory _options,
+        uint256 _duration,
+        address _owner,
+        string[] memory _countries
+    ) {
         if (_options.length < 2) {
             revert Poll__OptionCountMustBeGreaterThanTwo();
         }
@@ -90,14 +99,17 @@ contract Poll {
             revert Poll__MustLastLongerThanZero();
         }
 
-        s_title = _title;
-        s_options = _options;
-        i_governor = msg.sender;
-        i_owner = _owner;
-        s_state = State.CREATED;
-        s_duration = _duration;
-        s_endTime = 0;
-        s_startTime = 0;
+        title = _title;
+        options = _options;
+        GOVERNOR = msg.sender;
+        OWNER = _owner;
+        state = State.CREATED;
+        duration = _duration;
+        endTime = 0;
+        startTime = 0;
+        for (uint256 i = 0; i < _countries.length; i++) {
+            countriesAllowed[_countries[i]] = true;
+        }
     }
 
     // Management
@@ -105,37 +117,29 @@ contract Poll {
      * @notice Activate the poll. Sets startTime and endTime, moves state to Active.
      */
     function start() external onlyOwner inState(State.CREATED) {
-        s_startTime = block.timestamp;
-        s_endTime = block.timestamp + s_duration;
+        startTime = block.timestamp;
+        endTime = block.timestamp + duration;
 
-        s_state = State.ACTIVE;
-        emit PollStarted(s_startTime, s_endTime);
+        state = State.ACTIVE;
+        emit PollStarted(startTime, endTime);
     }
 
     /**
      * @notice Add a participant
      * @param _participant Address to register
      */
-    function addParticipant(address _participant) external onlyGovernor {
-        if (s_isRegistered[_participant]) {
+    function addParticipant(address _participant, string memory _country) external onlyGovernor {
+        if (isRegistered[_participant]) {
             revert Poll__UserAlreadyRegistered();
         }
 
-        s_isRegistered[_participant] = true;
-        s_participantList.push(_participant);
-        emit ParticipantAdded(_participant);
-    }
-
-    /**
-     * @notice Sets @param _participant as verified
-     */
-    function verifyParticipant(address _participant) external onlyGovernor {
-        if (s_isVerified[_participant]) {
-            revert Poll__UserAlreadyVerified();
+        if (!(countriesAllowed[_country])) {
+            revert Poll_UserWithInvalidNationality();
         }
 
-        s_isVerified[_participant] = true;
-        emit ParticipantVerified(_participant);
+        isRegistered[_participant] = true;
+        participantList.push(_participant);
+        emit ParticipantAdded(_participant);
     }
 
     // Voting
@@ -144,20 +148,20 @@ contract Poll {
      * @param _option Index of the chosen option
      */
     function castVote(address _voter, uint256 _option) external inState(State.ACTIVE) {
-        if (s_isRegistered[_voter]) {
+        if (isRegistered[_voter]) {
             revert Poll__UserAlreadyRegistered();
         }
 
-        if (s_hasVoted[_voter]) {
+        if (hasVoted[_voter]) {
             revert Poll__UserAlreadyVoted();
         }
 
-        if (!(_option < s_options.length)) {
+        if (!(_option < options.length)) {
             revert Poll__InvalidOption();
         }
 
-        s_hasVoted[_voter] = true;
-        s_votes[_option] += 1;
+        hasVoted[_voter] = true;
+        votes[_option] += 1;
         emit VoteCasted(_voter, _option);
     }
 
@@ -165,7 +169,7 @@ contract Poll {
      * @notice End the poll. Can be called by manager or automatically by timestamp.
      */
     function end() external inState(State.ACTIVE) {
-        s_state = State.ENDED;
+        state = State.ENDED;
         emit PollEnded(block.timestamp);
     }
 
@@ -174,11 +178,11 @@ contract Poll {
      * @notice Returns the vote counts for each option once poll has Ended.
      */
     function getResults() external view inState(State.ENDED) returns (uint256[] memory) {
-        uint256 len = s_options.length;
+        uint256 len = options.length;
         uint256[] memory results = new uint256[](len);
 
         for (uint256 i = 0; i < len; i++) {
-            results[i] = s_votes[i];
+            results[i] = votes[i];
         }
         return results;
     }
@@ -187,34 +191,34 @@ contract Poll {
      * @notice Retrieve number of registered participants.
      */
     function participantCount() external view returns (uint256) {
-        return s_participantList.length;
+        return participantList.length;
     }
 
     /**
      * @notice Retrieve the list of option labels.
      */
     function getOptions() external view returns (string[] memory) {
-        return s_options;
+        return options;
     }
 
     /**
      * @notice Retrieve the timestamp when voting ends
      */
     function getEndTime() external view returns (uint256) {
-        return s_endTime;
+        return endTime;
     }
 
     /**
      * @notice Retrieve the participants
      */
     function getRegisteredParticipants() external view returns (address[] memory) {
-        return s_participantList;
+        return participantList;
     }
 
     /**
      * @notice Retrieve the information weather a participant has voted already
      */
-    function hasVoted(address _voter) external view returns (bool) {
-        return s_hasVoted[_voter];
+    function hasUserVoted(address _voter) external view returns (bool) {
+        return hasVoted[_voter];
     }
 }
